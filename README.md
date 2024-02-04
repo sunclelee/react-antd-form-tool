160b0c7cf2982ea3951e18ad5e07642e4b6a6dbc
sunclelee <suncle@163.com>
2024-02-04 16:22:24 +0800

    初始化: 纯净的脚手架

---
+++ README.md
@@ -0,0 +1,14 @@
+基于ant design pro 脚手架实现的表单配置管理器
+
+# 环境
+node >= 12
+
+# 运行
+
+npm install
+
+npm start
+
+# 效果
+
+![图片的URL](https://github.com/sunclelee/d3-map-demo/blob/master/d3-map.gif?raw=true)
---
+++ src/pages/content/index.js
@@ -0,0 +1,16 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+
+class COM extends PureComponent {
+  state = {};
+
+  render() {
+    return <>NAME</>;
+  }
+}
+
+COM.defaultProps = {};
+
+COM.propTypes = {};
+
+export default COM;
